93a0977f2183c1f870de8edda23fec4ebbe6174e
Chare023 <branislavcharkic@gmail.com>
2022-12-27 12:36:35 +0100

    Added some movies in db.json

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let mainContainer = document.getElementById('mainContainer');
+
+console.log(mainContainer)
+
+fetch('http://localhost:3000/movies')
+    .then(res => res.json())
+    .then(movies => {
+        movies.forEach(movie => {
+            mainContainer.innerHTML += `
+                <div class="col">
+                    <div class="card h-100">
+                        <div class="card-header">
+                            <h5 class="card-title text-info text-center">${movie.title}</h5>
+                        </div>
+                        <div class="card-body">
+                            <img src="${movie.logo}" class="card-img-top" alt="...">
+                        </div>
+                        <div class="card-footer">
+                            <span class="text-secondary-emphasis">${movie.year}</span>
+                            <p class="card-text text-warning">&#9733; ${movie.rating}</p>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+    })
