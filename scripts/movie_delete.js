d19bbb75807486c9f8e5f16b01ca74833ebedefa
Chare023 <branislavcharkic@gmail.com>
2022-12-27 23:20:25 +0100

    added delete functionality

---
+++ scripts/movie_delete.js
@@ -0,0 +1,22 @@
+const queryString = location.href;
+
+function getQueryParams(url) {
+    const param = url.slice(url.indexOf('id') + 3);
+    return param;
+}
+
+let id = getQueryParams(queryString);
+let url = `http://localhost:3000/movies`;
+
+if (id) {
+    url += `/${id}`;
+    fetch(url, {
+        method: 'DELETE',
+        // headers: {
+        //     'Content-Type': 'application/json'
+        // },
+        // body: JSON.stringify(movie)
+    }).then(() => {
+        location.href = 'index.html';
+    })
+}
