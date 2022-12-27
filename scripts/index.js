b411dd30f874f11f58f526597522b810b42abe4a
Slou A <slowautomobile@protonmail.com>
2022-12-27 23:16:24 +0100

    cards edited

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
 let mainContainer = document.getElementById('mainContainer');
 
-console.log(mainContainer)
+console.log(mainContainer);
 
 fetch('http://localhost:3000/movies')
-    .then(res => res.json())
-    .then(movies => {
-        movies.forEach(movie => {
-            mainContainer.innerHTML += `
-                <div class="col">
-                    <div class="card border-dark h-100">
-                        <div class="card-header">
-                            <h5 class="card-title text-info text-center">${movie.title}</h5>
-                        </div>
-                        <div class="card-body">
-                            <img src="${movie.logo}" class="card-img-top" alt="...">
-                        </div>
-                        <div class="card-footer">
-                            <span class="text-secondary-emphasis">${movie.year}</span>
-                            <p class="card-text text-warning">&#9733; ${movie.rating}</p>
-                        </div>
-                    </div>
-                </div>
-            `
-        });
-    })
+  .then(res => res.json())
+  .then(movies => {
+    movies.forEach(movie => {
+      mainContainer.innerHTML += `
+				<div class="col">
+						<div class="card border-dark h-100">                   
+								<img src="${movie.logo}" class="card-img-top" alt="${movie.title} poster">
+								<div class="card-img-overlay justify-content-end">
+									<small class="card-text text-warning">&#9733; ${movie.rating}</small>
+								</div>
+								<div class="card-body">
+										<h5 class="d-inline card-title text-light-emphasis">${movie.title} </h5>
+										<p class="text-secondary">${movie.year}</p>
+								</div>
+						</div>
+				</div>
+		`;
+    });
+  });
+
+// <div class="card-footer">
+//
+//     <p class="card-text text-warning">&#9733; ${movie.rating}</p>
+// </div>
+
+// 
