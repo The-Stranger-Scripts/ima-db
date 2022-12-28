8cb13828485b91fc32e7f681edc7a8c9514fd392
Slou A <slowautomobile@protonmail.com>
2022-12-28 16:11:39 +0100

    added catalog posters

--- scripts/index.js
+++ scripts/index.js
@@ -7,25 +7,50 @@ fetch('http://localhost:3000/movies')
   .then(movies => {
     movies.forEach(movie => {
       mainContainer.innerHTML += `
-				<div class="col">
-						<div class="card border-dark h-100">                   
-								<img src="${movie.logo}" class="card-img-top" alt="${movie.title} poster">
-								<div class="card-img-overlay justify-content-end">
-									<small class="card-text text-warning">&#9733; ${movie.rating}</small>
-								</div>
-								<div class="card-body">
-										<h5 class="d-inline card-title text-light-emphasis">${movie.title} </h5>
-										<p class="text-secondary">${movie.year}</p>
-								</div>
-						</div>
-				</div>
+      <div class="col">
+      <div class="card border-dark h-100">
+        <div class="img-container">
+          <img
+            src="${movie.logo}"
+            class="card-img-top image" alt="">
+          <div class="middle">
+            <h4 class="rating text-warning"><span class="me-1">&#9733;</span>${movie.rating}</h4>
+            <a href='movie_view.html?id=${movie.id}' type="button" class="btn btn-info text">Detaljnije</a>
+
+            <div id="buttons-wrap">
+              <a href="movie_edit.html?id=${movie.id}" class="text-warning-emphasis link-warning edit-button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path
+                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                  <path fill-rule="evenodd"
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                </svg>
+              </a>
+
+              <a href="movie_delete.html?id=${movie.id}" class="text-danger-emphasis link-danger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-trash" viewBox="0 0 16 16">
+                  <path
+                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                  <path fill-rule="evenodd"
+                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="card-body">
+          <h5 class="card-title text-light-emphasis">${movie.title}</h5>
+          <p class="text-secondary">${movie.year}</p>
+        </div>
+      </div>
+    </div>
+
+
+				
 		`;
     });
   });
-
-// <div class="card-footer">
-//
-//     <p class="card-text text-warning">&#9733; ${movie.rating}</p>
-// </div>
-
-// 
